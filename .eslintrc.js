d98db3ab90ba72070fd44fcd0c8e69561e431034
davidhcefx <davidhu0903ex3@gmail.com>
2022-08-14 17:48:46 +0800

    Fix eslint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,12 +13,12 @@ module.exports = {
   rules: {
     'no-multi-spaces': [
       'error',
-      { "ignoreEOLComments": true },  // we need two spaces before comment
+      { ignoreEOLComments: true },  // we need two spaces before comment
     ],
     'quote-props': 'warn',  // quoted object properties is accepted
     'linebreak-style': 'warn',  // CRLF linebreaks are accepted
     'nonblock-statement-body-position': 'warn',  // allow statement below if
-    'curly': ['error', 'multi-line', 'consistent'],  // enforce curly except single line
+    curly: ['error', 'multi-line', 'consistent'],  // enforce curly except single line
     'no-console': 'off',  // node.js project
   },
 };
